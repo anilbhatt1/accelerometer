c4293ead8e89e30a67b2a6bcd733656579862274
anilbhatt1 <anilaknb@gmail.com>
2023-06-16 16:09:54 +0530

    Modified the ios code handling to add onclick

--- src/App.js
+++ src/App.js
@@ -4,31 +4,33 @@ const App = () => {
   const [acceleration, setAcceleration] = useState({ x: 0, y: 0, z: 0 });
   const isAndroid = /Android/.test(navigator.userAgent);
   const isiOS = /iPhone|iPad|iPod/.test(navigator.userAgent);
-  console.log('isAndroid', isAndroid)
-  console.log('isiOS', isiOS)
+  const isNone = !isAndroid && !isiOS;
+  const isFunc = (typeof DeviceMotionEvent.requestPermission === 'function')
+
+  const handlePermissionRequest = () => {
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+      DeviceMotionEvent.requestPermission()
+        .then((response) => {
+          if (response === 'granted') {
+            window.addEventListener('devicemotion', handleMotionEvent);
+          }
+        })
+        .catch((error) => {
+          console.error('Error requesting accelerometer permission:', error);
+        });
+    }
+  };
+
+  const handleStartRequest = () => {
+            window.addEventListener('devicemotion', handleMotionEvent);    
+  };
+
+  const handleMotionEvent = (event) => {
+    const { x, y, z } = event.accelerationIncludingGravity;
+    setAcceleration({ x, y, z });
+  };
 
   useEffect(() => {
-    const handleMotionEvent = (event) => {
-      const { x, y, z } = event.accelerationIncludingGravity;
-      setAcceleration({ x, y, z });
-    };
-
-    if (isiOS) {
-      // Running on iOS, ask for accelerometer permission
-        DeviceMotionEvent.requestPermission()
-          .then((response) => {
-            if (response === 'granted') {
-              window.addEventListener('devicemotion', handleMotionEvent);
-            }
-          })
-          .catch((error) => {
-            console.error('Error requesting accelerometer permission:', error);
-          });
-    } else {
-      // Running on Android or non IOS, directly add event listener
-      window.addEventListener('devicemotion', handleMotionEvent);
-    }
-
     return () => {
       window.removeEventListener('devicemotion', handleMotionEvent);
     };
@@ -36,12 +38,17 @@ const App = () => {
 
   return (
     <div>
-      <h1>Android: {isAndroid ? 'Yes' : 'No'}</h1>
-      <h1>ios: {isiOS ? 'Yes' : 'No'}</h1>
-      <h1>Accelerometer Data</h1>
-      <p>X : {acceleration.x}</p>
-      <p>Y : {acceleration.y}</p>
-      <p>Z : {acceleration.z}</p>
+        <h1>Accelerometer Data</h1>
+        <p>X: {acceleration.x}</p>
+        <p>Y: {acceleration.y}</p>
+        <p>Z: {acceleration.z}</p>
+        <p>isFunc : {typeof DeviceMotionEvent.requestPermission === 'function' ? "Yes" : "No"}</p>
+        <p>isAndroid : {isAndroid ? "Yes" : "No"}</p>
+        <p>isiOS : {isiOS ? "Yes" : "No"}</p>
+        <p>isNone : {isNone ? "Yes" : "No"}</p>
+       {isAndroid && <button onClick={handleStartRequest}>Android - Start</button>}              
+       {isiOS && <button onClick={handlePermissionRequest}>iOS - Permission & Start</button>}
+       {isNone && <button onClick={handleStartRequest}>Non Android & non iOS - Start</button>}
     </div>
   );
 };
